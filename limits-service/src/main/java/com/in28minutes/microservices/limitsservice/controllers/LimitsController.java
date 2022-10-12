2835f1c85ca38e7345e1a40a52376e225cbb519b
Leonardo <leonardocg9492@gmail.com>
2022-10-12 10:23:59 -0500

    currency-exchange-microservice created

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controllers/LimitsController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controllers/LimitsController.java
@@ -1,6 +1,5 @@
 package com.in28minutes.microservices.limitsservice.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
