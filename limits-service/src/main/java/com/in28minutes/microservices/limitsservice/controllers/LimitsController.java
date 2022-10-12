68a576c1899e6f55303352f3a913e5a9ff88ab4a
Leonardo <leonardocg9492@gmail.com>
2022-10-11 23:10:18 -0500

    first commit

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/controllers/LimitsController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.microservices.limitsservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.microservices.limitsservice.configuration.Configuration;
+import com.in28minutes.microservices.limitsservice.models.Limits;
+
+@RestController
+public class LimitsController {
+  
+  private Configuration configuration;
+
+  public LimitsController(Configuration configuration) {
+    this.configuration = configuration;
+  }
+
+  @GetMapping("/limits")
+  public Limits retrieveLimits() {
+    return new Limits(configuration.getMinimum(), configuration.getMaximum());
+  }
+}
